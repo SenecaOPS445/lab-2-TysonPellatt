7cb59a9ab0b533b90b7349f3b57c76693a1853ad
Tyson Pellatt <tpellatt@myseneca.ca>
2025-01-22 18:14:32 -0500

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"Author: Tyson Darius Pellatt"
+"Author ID: tpellatt"
+"Date Created: 2025/01/22"
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+   print(timer)
+   timer -= 1
+print("blast off!")
